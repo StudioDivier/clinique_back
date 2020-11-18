745639f29064d20656c8b4a8fec89671228621e2
StudioDivier <ianfoxstudio@gmail.com>
2020-11-18 17:38:38 +0300

    v2.3-herokuDB

--- clinique/settings.py
+++ clinique/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'clinique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'clinique',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'clinique',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
