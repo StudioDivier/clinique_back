d96b047af8a61e47275738a74f9b986a6baa526f
StudioDivier <ianfoxstudio@gmail.com>
2020-11-18 17:58:17 +0300

    v3.1

--- clinique/settings.py
+++ clinique/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'wk_l^9)sndvffhp!$xcffix^p^7ccydj*(3+!duq!1$y@z!0i2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
