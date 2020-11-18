050b8c41352608bc827003363429e5b21c126886
StudioDivier <ianfoxstudio@gmail.com>
2020-11-18 17:54:05 +0300

    v3.0

--- clinique/settings.py
+++ clinique/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wk_l^9)sndvffhp!$xcffix^p^7ccydj*(3+!duq!1$y@z!0i2'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'clinique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'clinique',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd3lcbelp96r1f6',
+        'USER': 'lswtcxfbskyrzl',
+        'PASSWORD': '770e8e972f7fb1247f07a1df822016dc6054361a858f4d8a7590a53b63b550e3',
+        'HOST': 'ec2-3-220-98-137.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
